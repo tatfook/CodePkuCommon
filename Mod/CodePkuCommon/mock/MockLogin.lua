2f17ac19bba9cc60b94a55844f5523c896680c89
junm <huangjm@codepku.com>
2020-07-17 09:35:36 +0800

    fix bug

--- Mod/CodePkuCommon/mock/MockLogin.lua
+++ Mod/CodePkuCommon/mock/MockLogin.lua
@@ -15,20 +15,20 @@ function MockLogin:login()
 
     ApiService.Login(codepkuMobile,codepkuPasswd):next(function (response)
 
-        if type(response.data) ~= "table" then            
+        if type(response.data) ~= "table" or type(response.data.data) ~= "table" then            
             GameLogic.AddBBS(nil, L"Codepku自动登录-服务器连接失败", 3000, "255 0 0", 21)
             return false
         end
 
-        local token = response.data.token or System.User.codepkuToken
-        local userId = response.data.id or 0
-        local nickname = response.data.nickname or ""
-        local mobile = response.data.mobile or ""
+        local token = response.data.data.token or System.User.codepkuToken
+        local userId = response.data.data.id or 0
+        local nickname = response.data.data.nickname or ""
+        local mobile = response.data.data.mobile or ""
 
         commonlib.setfield("System.User.codepkuToken", token)
         commonlib.setfield("System.User.mobile", mobile)
         commonlib.setfield("System.User.username", mobile)
-        commonlib.setfield('system.User.id', userId)            
+        commonlib.setfield('System.User.id', userId)            
         commonlib.setfield("System.User.nickName", nickname)
 
         GameLogic.AddBBS(nil, L"Codepku自动登录成功，当前账号" .. codepkuMobile, 3000, "0 255 0", 21)
